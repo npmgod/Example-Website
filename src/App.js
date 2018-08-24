2df57c72f382ea7170c57d49f024327149973b5e
Gage Henderson <gage@quangoinc.com>
2018-08-24 16:27:25 -0700

    Okay I think I fixed it

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     console.log(process.env.PUBLIC_URL);
-    return <div className="App">asdf;lkajs;dlfkjas;ldkjf</div>;
+    return <div className="App" />;
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import './styles/_fonts.css';
 import './styles/_global.css';
 import './styles/font-awesome/css/all.css';
@@ -12,13 +12,13 @@ import TrumpTweets from './pages/TrumpTweets';
 import CorporateIpsum from './pages/CorporateIpsum';
 
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter basename={process.env.PUBLIC_URL}>
     <div>
       <Route path="/" component={App} />
       <Route path="/trump-tweets" component={TrumpTweets} />
       <Route path="/corporate-ipsum" component={CorporateIpsum} />
     </div>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
 registerServiceWorker();
